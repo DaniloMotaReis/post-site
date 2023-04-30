053ac96b34985005a0ec898ce0324097722ce4c5
Danilo Mota Reis <101838987+DaniloMotaReis@users.noreply.github.com>
2023-04-29 21:01:51 -0300

    db nuvem

--- models/db.js
+++ models/db.js
@@ -1,10 +1,16 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('postapp','testepots','teste123',{
-    host: "db4free.net",
+const sequelize = new Sequelize('sql10614849','sql10614849','WuqWKvXEQ5',{
+    host: "sql10.freemysqlhosting.net",
     port: "3306",
     dialect: 'mysql'
 });
+
+/*const sequelize = new Sequelize('postapp','root','root',{
+    host: "localhost",
+    port: "3306",
+    dialect: 'mysql'
+});*/
 
 module.exports = {
     Sequelize: Sequelize,
